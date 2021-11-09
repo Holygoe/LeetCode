0c92abc517ecf44f6a3c9775f5c1a64daf00f172
Kondratev Denis <holygoe@gmail.com>
2021-11-09 11:30:11 +0300

    Add 'Unique Binary Search Trees'

---
+++ UniqueBinarySearchTrees/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace UniqueBinarySearchTrees
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            //BenchmarkRunner.Run<Benchmarks>();
+
+            Test(1, 1);
+            Test(2, 2);
+            Test(3, 5);
+            Test(4, 14);
+            Test(5, 42);
+            Test(6, 132);
+            Test(7, 429);
+        }
+
+        private static void Test(int input, int expected)
+        {
+            var solutions = new Solutions();
+            var result = solutions.NumTreesNonAlloc(input);
+            
+            Console.WriteLine($"Input: {input}, result: {result}, expect: {expected} — {result == expected}");
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        private const int Min = 1, Max = 30;
+        
+        [Benchmark]
+        public int NumTrees()
+        {
+            var solutions = new Solutions();
+            
+            for (var i = Min; i < Max; i++)
+            {
+                solutions.NumTrees(i);
+            }
+        
+            return 0;
+        }
+        
+        [Benchmark]
+        public void NumTreesNonAlloc()
+        {
+            var solutions = new Solutions();
+            
+            for (var i = Min; i < Max; i++)
+            {
+                solutions.NumTreesNonAlloc(i);
+            }
+        }
+        
+        [Benchmark]
+        public void NumTreesStackAlloc()
+        {
+            var solutions = new Solutions();
+            
+            for (var i = Min; i < Max; i++)
+            {
+                solutions.NumTreesStackAlloc(i);
+            }
+        }
+    }
+}
---
+++ UniqueBinarySearchTrees/Solutions.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace UniqueBinarySearchTrees
+{
+    public class Solutions
+    {
+        public int NumTrees(int n)
+        {
+            var row = new int[n + 1];
+            row[0] = 1;
+            row[1] = 1;
+        
+            for (var i = 2; i <= n; i++)
+            {
+                var sum = 0;
+                var left = i - 1;
+                var right = 0;
+            
+                while (left >= right)
+                {
+                    var step = row[left] * row[right];
+                
+                    if (left != right)
+                    {
+                        step *= 2;
+                    }
+
+                    sum += step;
+                
+                    left--;
+                    right++;
+                }
+            
+                row[i] = sum;
+            }
+        
+            return row[n];
+        }
+        
+        public int NumTreesNonAlloc(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            
+            var sum = 0;
+            var left = n - 1;
+            var right = 0;
+            
+            while (left >= right)
+            {
+                var step = NumTreesNonAlloc(left) * NumTreesNonAlloc(right);
+                
+                if (left != right)
+                {
+                    step *= 2;
+                }
+
+                sum += step;
+                
+                left--;
+                right++;
+            }
+        
+            return sum;
+        }
+        
+        public unsafe int NumTreesStackAlloc(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            
+            var row = stackalloc int[n + 1];
+            row[0] = 1;
+            row[1] = 1;
+        
+            for (var i = 2; i <= n; i++)
+            {
+                var sum = 0;
+                var left = i - 1;
+                var right = 0;
+            
+                while (left >= right)
+                {
+                    var step = row[left] * row[right];
+                
+                    if (left != right)
+                    {
+                        step *= 2;
+                    }
+
+                    sum += step;
+                
+                    left--;
+                    right++;
+                }
+                
+                row[i] = sum;
+            }
+        
+            return row[n];
+        }
+        
+        private static unsafe int GetRowItem(int i, int* row)
+        {
+            var sum = 0;
+            var left = i - 1;
+            var right = 0;
+            
+            while (left >= right)
+            {
+                var step = row[left] * row[right];
+                
+                if (left != right)
+                {
+                    step *= 2;
+                }
+
+                sum += step;
+                
+                left--;
+                right++;
+            }
+
+            return sum;
+        }
+        
+        public static string timeConversion(string s)
+        {
+            var dt = DateTime.ParseExact(s, "h:mm:ss", CultureInfo.InvariantCulture);
+        
+            return dt.ToString("hh:mm:ss");
+        }  
+
+    }
+}
