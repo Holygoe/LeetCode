bc99253f0329ec0a941d8ea19a4a79274859b4f5
Kondratev Denis <holygoe@gmail.com>
2021-11-09 22:04:55 +0300

    Number Of Valid Words For Each Puzzle

---
+++ NumberOfValidWordsForEachPuzzle/HashSolution.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace NumberOfValidWordsForEachPuzzle
+{
+    public class HashSolution
+    {
+        private const int ACode = 'a';
+
+        public IList<int> FindNumOfValidWords(string[] words, string[] puzzles)
+        {
+            var result = new int[puzzles.Length];
+            var wordHashMap = new Dictionary<int, int>();
+            
+            foreach (var word in words)
+            {
+                var wordBitmap = GetBitmap(word);
+
+                if (!wordHashMap.TryAdd(wordBitmap, 1))
+                {
+                    wordHashMap[wordBitmap]++;
+                }
+            }
+            
+            for (var i = 0; i < puzzles.Length; i++)
+            {
+                var puzzle = puzzles[i];
+                var firstLetter = 1 << (puzzle[0] - ACode);
+                
+                if (!wordHashMap.TryGetValue(firstLetter, out var count))
+                {
+                    count = 0;
+                }
+                
+                var mask = GetBitmap(puzzle, 1);
+                
+                for (var subMask = mask; subMask > 0; subMask = (subMask - 1) & mask) 
+                {
+                    if (wordHashMap.TryGetValue(subMask | firstLetter, out var value))
+                    {
+                        count += value;
+                    }
+                }
+
+                result[i] = count;
+            }
+            
+            return result;
+        }
+
+        private static int GetBitmap(string source, int startLetter = 0)
+        {
+            var bitmap = 0;
+            var sourceLength = source.Length;
+            
+            for (var i = startLetter; i < sourceLength; i++)
+            {
+                bitmap |= 1 << (source[i] - ACode);
+            }
+            
+            return bitmap;
+        }
+    }
+}
---
+++ NumberOfValidWordsForEachPuzzle/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace NumberOfValidWordsForEachPuzzle
+{
+    /// <summary>
+    /// The problem https://leetcode.com/problems/number-of-valid-words-for-each-puzzle/
+    /// </summary>
+    public class Solution
+    {
+        private const int ACode = 'a';
+
+        public IList<int> FindNumOfValidWords(string[] words, string[] puzzles)
+        {
+            var wordCount = words.Length;
+            var puzzleCount = puzzles.Length;
+            
+            var puzzleMaps = new int[puzzleCount];
+            var firstLetterBits = new int[puzzleCount];
+            var result = new int[puzzleCount];
+            
+            for (var i = 0; i < puzzleCount; i++)
+            {
+                firstLetterBits[i] = 1 << (puzzles[i][0] - ACode);
+                puzzleMaps[i] = ~GetBitmap(puzzles[i]);
+            }
+
+            for (var i = 0; i < wordCount; i++)
+            {
+                var wordMap = GetBitmap(words[i]);
+
+                for (var j = 0; j < puzzleCount; j++)
+                {
+                    if ((wordMap & firstLetterBits[j]) == 0
+                        || (wordMap & puzzleMaps[j]) != 0)
+                    {
+                        continue;
+                    }
+
+                    result[j]++;
+                }
+            }
+
+            return result;
+        }
+
+        private static int GetBitmap(string source)
+        {
+            var bitmap = 0;
+            var sourceLength = source.Length;
+            
+            for (var i = 0; i < sourceLength; i++)
+            {
+                bitmap |= 1 << (source[i] - ACode);
+            }
+            
+            return bitmap;
+        }
+    }
+}
