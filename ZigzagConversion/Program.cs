bc7c929090bbc9719188249e43b850ecebab9305
Kondratev Denis <holygoe@gmail.com>
2021-11-10 20:26:55 +0300

    Zigzag Conversion

---
+++ ZigzagConversion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ZigzagConversion
+{
+    internal static class Program
+    {
+        private static readonly Task[] Tasks =
+        {
+            new Task { s = "PAYPALISHIRING", numRows = 3, expectation = "PAHNAPLSIIGYIR" },
+            new Task { s = "PAYPALISHIRING", numRows = 4, expectation = "PINALSIGYAHRPI" },
+            new Task { s = "A", numRows = 1, expectation = "A" }
+        };
+        
+        private static void Main()
+        {
+            var solution = new Solution();
+
+            foreach (var task in Tasks)
+            {
+                Test(solution, task);
+            }
+        }
+
+        private static void Test(Solution solution, Task task)
+        {
+            var result = solution.Convert(task.s, task.numRows);
+            
+            Console.WriteLine($"s '{task.s}', rows {task.numRows}, result '{result}', expect '{task.expectation}' — {result == task.expectation}");
+        }
+    }
+
+    public class Task
+    {
+        public string s;
+        public int numRows;
+        public string expectation;
+    }
+}
---
+++ ZigzagConversion/Solution.cs
@@ -0,0 +1,57 @@
+﻿namespace ZigzagConversion
+{
+    public class Solution
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows == 1) return s;
+            
+            var result = string.Create(s.Length, s, (result, origin) =>
+            {
+                var i = 0;
+                var step = (numRows - 1) * 2;
+                var j = 0;
+
+                while (j < origin.Length)
+                {
+                    result[i] = origin[j];
+                    i++;
+                    j += step;
+                }
+
+                if (numRows > 2)
+                {
+                    for (var k = 1; k < numRows - 1; k++)
+                    {
+                        int firstStep = step - k * 2, secondStep = k * 2;
+                        j = k;
+                            
+                        while (j < origin.Length)
+                        {
+                            result[i] = origin[j];
+                            i++;
+                            j += firstStep;
+
+                            if (j >= origin.Length) break;
+
+                            result[i] = origin[j];
+                            i++;
+                            j += secondStep;
+                        }
+                    }
+                }
+
+                j = numRows - 1;
+                
+                while (j < origin.Length)
+                {
+                    result[i] = origin[j];
+                    i++;
+                    j += step;
+                }
+            });
+            
+            return result;
+        }
+    }
+}
