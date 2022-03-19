930640a477d2d95f72f9b7050c3d1e5e5e3e5064
Denis Kondratev <kondratev.da@gmail.com>
2022-03-20 00:48:53 +0300

    MaximumFrequencyStack

---
+++ MaximumFrequencyStack/FreqStack.cs
@@ -0,0 +1,55 @@
+﻿namespace MaximumFrequencyStack;
+
+public class FreqStack
+{
+    private readonly Dictionary<int, int> _map;
+    private readonly Dictionary<int, Stack<int>> _frequency;
+    private int _maxFrequency;
+
+    public FreqStack()
+    {
+        _map = new();
+        _frequency = new();
+    }
+    
+    public void Push(int value)
+    {
+        if (_map.TryGetValue(value, out var valueFrequency))
+        {
+            valueFrequency++;
+            _map[value] = valueFrequency;
+        }
+        else
+        {
+            valueFrequency = 1;
+            _map.Add(value, valueFrequency);
+        }
+
+        if (!_frequency.TryGetValue(valueFrequency, out var frequencyStack))
+        {
+            frequencyStack = new();
+            _frequency.Add(valueFrequency, frequencyStack);
+        }
+        
+        frequencyStack.Push(value);
+
+        if (valueFrequency > _maxFrequency)
+        {
+            _maxFrequency = valueFrequency;
+        }
+    }
+
+    public int Pop()
+    {
+        var resultStack = _frequency[_maxFrequency];
+        var result = resultStack.Pop();
+        _map[result]--;
+        
+        if (resultStack.Count == 0)
+        {
+            _maxFrequency--;
+        }
+
+        return result;
+    }
+}
---
+++ MaximumFrequencyStack/Program.cs
@@ -0,0 +1,13 @@
+﻿using MaximumFrequencyStack;
+
+var stack = new FreqStack();
+stack.Push(5);
+stack.Push(7);
+stack.Push(5);
+stack.Push(7);
+stack.Push(4);
+stack.Push(5);
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
