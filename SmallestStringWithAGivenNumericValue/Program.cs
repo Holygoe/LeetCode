9c47965fd8c3723774368851f2f5d55e531ba532
Denis Kondratev <kondratev.da@gmail.com>
2022-03-22 21:24:41 +0300

    1663. Smallest String With A Given Numeric Value

---
+++ SmallestStringWithAGivenNumericValue/Program.cs
@@ -0,0 +1,6 @@
+﻿using SmallestStringWithAGivenNumericValue;
+
+var solution = new Solution();
+
+Console.WriteLine("For 3, 27 expects 'aay', result: " + solution.GetSmallestString(3, 27));
+Console.WriteLine("For 5, 73 expects 'aaszz', result: " + solution.GetSmallestString(5, 73));
---
+++ SmallestStringWithAGivenNumericValue/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace SmallestStringWithAGivenNumericValue;
+
+public class Solution
+{
+    public string GetSmallestString(int n, int k)
+    {
+        const int maxNum = 'z' - 'a' + 1;
+        var nums = new int[n];
+
+        for (var i = nums.Length - 1; i >= 0; i--)
+        {
+            var rest = k - i;
+            nums[i] = rest > maxNum ? maxNum : rest;
+            k -= nums[i];
+        }
+        
+        var result = string.Create(n, nums, (str, array) =>
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                str[i] = (char)('a' + array[i] - 1);
+            }
+        });
+
+        return result;
+    }
+}
